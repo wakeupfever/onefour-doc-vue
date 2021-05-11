6fb1b496af5b83fdf4a415d8d6378acaa8a97874
lu_li <1874803565@qq.com>
2021-05-11 22:10:50 +0800

    fix: 优化检测配置

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
-import { InjectionKey, IAllType, IAllModules } from "@vue/runtime-core"
-import { createStore, Store, useStore as baseUseStore } from "vuex"
+import { InjectionKey, IAllType, IAllModules } from '@vue/runtime-core'
+import { createStore, Store, useStore as baseUseStore } from 'vuex'
 
 import { docStore } from './modules/doc'
 import { testStore } from './modules/test'
@@ -9,8 +9,8 @@ export const key: InjectionKey<Store<IAllModules>> = Symbol()
 export const store: Store<IAllType> = createStore<IAllType>({
   modules: {
     docStore,
-    testStore
-  }
+    testStore,
+  },
 })
 
 export function useStore() {
