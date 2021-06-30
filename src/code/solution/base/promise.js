ae580e4db466bfcd5ee0454c8886f4745cb9a92a
Five <1874803565@qq.com>
2021-06-30 11:00:50 +0800

    feat: 增加promise注解

--- src/code/solution/base/promise.js
+++ src/code/solution/base/promise.js
@@ -15,9 +15,30 @@ function resolvePromise(promise, x, resolve, reject) {
   }
 }
 
+/**
+ * 1.核心逻辑实现
+ * 2.加入异步操作
+ * 3.实现then方法多次调用
+ * 4.实现then链式调用
+ * 5.then链式调用识别promise
+ * 6.捕获错误及then链式调用其他状态
+ * @export
+ * @class PromiseFns
+ */
 export class PromiseFns {
   constructor(executor) {
     executor(this.resolve, this.reject)
+  }
+
+  static resolve(parameter) {
+    if (parameter in PromiseFns) {
+      return parameter
+    }
+    return new PromiseFns((resolve) => resolve(parameter))
+  }
+
+  static reject(reason) {
+    return new PromiseFns((resolve, reject) => reject(reason))
   }
 
   // promise 的状态
@@ -56,6 +77,14 @@ export class PromiseFns {
   }
 
   then(onFulfilled, onRejected) {
+    onFulfilled =
+      typeof onFulfilled === 'function' ? onFulfilled : (value) => value
+    onRejected =
+      typeof onRejected === 'function'
+        ? onRejected
+        : (reason) => {
+            throw reason
+          }
     const promise = new PromiseFns((resolve, reject) => {
       if (this.status === FULFILLED) {
         queueMicrotask(() => {
@@ -65,8 +94,24 @@ export class PromiseFns {
       } else if (this.status === REJECTED) {
         onRejected(this.value)
       } else if (this.status === PENDING) {
-        this.onFulfilledCallback.push(onFulfilled)
-        this.onRejectedCallback.push(onRejected)
+        this.onFulfilledCallback.push(() => {
+          queueMicrotask(() => {
+            try {
+              const x = onFulfilled(this.value)
+              resolvePromise(promise, x, resolve, reject)
+            } catch (error) {
+              reject(error)
+            }
+          })
+        })
+        this.onRejectedCallback.push(() => {
+          try {
+            const x = onRejected(this.reason)
+            resolvePromise(promise, x, resolve, reject)
+          } catch (error) {
+            reject(error)
+          }
+        })
       }
     })
     return promise
